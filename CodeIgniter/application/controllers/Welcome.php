1d9c325c6d476be020b1af0c1218ef06265bfa7a
kharbandavikram <vikramkharbanda@webspintechnologies.com>
2021-09-23 17:44:59 +0530

    pdf

--- CodeIgniter/application/controllers/Welcome.php
+++ CodeIgniter/application/controllers/Welcome.php
@@ -20,7 +20,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->library('pdf');
+$this->load->library('pdf');
 $this->pdf->load_view('mypdf');
 $this->pdf->render();
 $this->pdf->stream("welcome.pdf");
