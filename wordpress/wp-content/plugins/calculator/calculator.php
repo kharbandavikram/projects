383ebb857aec0d80bef33f9db620375a03013048
kharbandavikram <vikramkharbanda@webspintechnologies.com>
2021-10-01 17:22:48 +0530

    no message

--- wordpress/wp-content/plugins/calculator/calculator.php
+++ wordpress/wp-content/plugins/calculator/calculator.php
@@ -27,13 +27,13 @@ function Calculator_menu_item()
 	add_action('wp_enqueue_scripts','plugin_css_jsscripts');
 function plugin_css_jsscripts() {
    // CSS
-   wp_enqueue_style( 'style-css', plugins_url( '/style.css', __FILE__ ));
+   wp_enqueue_style('style-css', plugins_url( '/style.css', __FILE__ ));
 
    // JavaScript
-   wp_enqueue_script( 'script', plugins_url( '/js/script.js', __FILE__ ),array('jquery'));
+   wp_enqueue_script('script', plugins_url( '/js/script.js', __FILE__ ),array('jquery'));
 
    // Pass ajax_url to script.js
-   wp_localize_script( 'script-js', 'plugin_ajax_object',
+   wp_localize_script('script-js', 'plugin_ajax_object',
    array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
 }
 
