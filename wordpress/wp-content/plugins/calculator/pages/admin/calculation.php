ef12a4a717760787b20721d742a4ee0b1b07138d
kharbandavikram <vikramkharbanda@webspintechnologies.com>
2021-10-01 12:00:04 +0530

    no message

--- wordpress/wp-content/plugins/calculator/pages/admin/calculation.php
+++ wordpress/wp-content/plugins/calculator/pages/admin/calculation.php
@@ -1,5 +1,33 @@
 <?php
 
-echo "Hello";
 
 ?>
+
+Select Park<select>
+
+<option value="Animal Kingdom">Animal Kingdom </option>
+<option value ="Diney Park">Disney Park </option>
+
+</select>
+
+
+No. of Days Selected <select>
+
+<option value="1">1 </option>
+<option value ="2">2 </option>
+<option value ="3">3 </option>
+<option value ="4">4 </option>
+<option value ="5">5 </option>
+</select>
+
+
+<input type="button" onclick="click_calculate()">
+
+
+<script>
+
+function click_calculate()
+{
+	alert('test calculation');
+}
+</script>
