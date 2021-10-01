259baa748a41ae066d5d7bcecfbaf7dced3519f2
kharbandavikram <vikramkharbanda@webspintechnologies.com>
2021-10-01 11:40:55 +0530

    no message

--- wordpress/wp-content/plugins/calculator/calculator.php
+++ wordpress/wp-content/plugins/calculator/calculator.php
@@ -8,4 +8,20 @@ Plugin Name:  Calculation Plugin
 */
 
 
-?>
+
+
+function Calculator_menu_item()
+{
+		add_menu_page( 'Calculation', 'Calculation Item', 'manage_options', 'calculate','manage_calculation','' );
+		  
+}
+
+	add_action( 'admin_menu', 'Calculator_menu_item' );
+	
+	function manage_calculation()
+	{
+		include('pages/admin/calculation');
+	}
+	
+	
+	?>
